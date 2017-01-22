283b3aab6caadffc9fdf15629eb3eb02827638fc
Saleema Ali <sali1390@gmail.com>
2017-01-21 21:04:32 -0500

    Post requests going through

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -5,4 +5,7 @@ module.exports = function(app){
     app.get('/api/friends', function(req, res){
         res.json(friendsData);
     })
+    app.post('/api/friends', function(req, res){
+        friendsData.push(req.body);
+    })
 }
--- server.js
+++ server.js
@@ -4,8 +4,10 @@ var path = require('path');
 var app = express();
 var PORT = 3000;
 
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(bodyParser.json())
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.text());
+app.use(bodyParser.json({ type: "application/vnd.api+json" }));
 
 require('./app/routing/apiRoutes.js')(app);
 require('./app/routing/htmlRoutes.js')(app);
