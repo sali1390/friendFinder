17a0ba2c8541f1144092641e7ed138b882b78ee8
Saleema Ali <sali1390@gmail.com>
2017-01-21 19:16:21 -0500

    Updated app with dummy data and routing functionality

---
+++ server.js
@@ -0,0 +1,15 @@
+var express = require('express');
+var bodyParser = require('body-parser');
+var path = require('path');
+var app = express();
+var PORT = 3000;
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json())
+
+require('./app/routing/apiRoutes.js')(app);
+require('./app/routing/htmlRoutes.js')(app);
+
+app.listen(PORT, function(err){
+    console.log("App Listening")
+})
