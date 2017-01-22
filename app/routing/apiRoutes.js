73900f82d9f718e365437222b526fc6b81c39f5b
Saleema Ali <sali1390@gmail.com>
2017-01-22 00:38:48 -0500

    Updated api routes

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -6,6 +6,15 @@ module.exports = function(app){
         res.json(friendsData);
     })
     app.post('/api/friends', function(req, res){
+        var scores = (req.body.scores);
+        var scoreTotal=0;
+        
+        for(var i=0; i<scores.length; i++){
+            scoreTotal += parseInt(scores[i]);
+            
+            console.log(scoreTotal)
+        }
+        
         friendsData.push(req.body);
     })
 }
